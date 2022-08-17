9238c4c130e3bc71b4c20acdf402722135ffe0ec
Andrey-Bondarevskij <aloxnesir@gmail.com>
2022-08-17 20:10:42 +0300

    classCom=>functionalCom

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,58 +1,108 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useMemo } from 'react';
 import FeedbackOptions from './molecules/FeedbackOptions/FeedbackOptions';
 import Statistics from './molecules/Statistics/Statistics';
 import Title from './atoms/Title/Title';
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const onLeaveFeedback = name => {
+    if (name === 'good') {
+      setGood(prevGood => {
+        return prevGood + 1;
+      });
+    }
+    if (name === 'bad') {
+        setBad(prevBad => {
+          return prevBad + 1;
+        });
+     }
+    if (name === 'neutral') {
+        setNeutral(prevNeutral => {
+          return prevNeutral + 1;
+        });
+    }
   };
-  onLeaveFeedback =( name) => {
-    this.setState((prevState) => {
-     return {[name] : prevState[name] + 1} 
-    })
-  }
-  
-  onCountTotalFeedback = () => {
-    return (
-      this.state.good +
-      this.state.neutral +
-      this.state.bad
-    );
+  const onCountTotalFeedback = () => {
+    return good + neutral + bad;
   };
-  onCountPositiveFeedbackPercentage = () => {
-    const total =
-      this.state.good +
-      this.state.neutral +
-      this.state.bad;
-    const precentage = Math.round((this.state.good / total) * 100);
+  const onCountPositiveFeedbackPercentage = () => {
+    const total = good + neutral + bad;
+    const precentage = Math.round((good / total) * 100);
     return `${precentage} %`;
   };
-
-  render() {
-    return (
-      <>
-        <FeedbackOptions
-          onLeaveFeedback={this.onLeaveFeedback}
-          options={Object.keys(this.state)}
+  return (
+    <>
+      <FeedbackOptions
+        onLeaveFeedback={onLeaveFeedback}
+        options={[`good`,`neutral`,`bad`]}
+      />
+      {good + neutral + bad > 0 ? (
+        <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={onCountTotalFeedback()}
+          positivePercentage={onCountPositiveFeedbackPercentage()}
         />
-        {this.state.good +
-          this.state.neutral +
-          this.state.bad >
-        0 ? (
-          <Statistics
-            good={this.state.good}
-            neutral={this.state.neutral}
-            bad={this.state.bad}
-            total={this.onCountTotalFeedback()}
-            positivePercentage={this.onCountPositiveFeedbackPercentage()}
-          />
-        ) : (
-          <Title titleText="There is no feedback" />
-        )}
-      </>
-    );
-  }
-}
+      ) : (
+        <Title titleText="There is no feedback" />
+      )}
+    </>
+  );
+};
+
+// export class App extends Component {
+//   state = {
+//     good: 0,
+//     neutral: 0,
+//     bad: 0,
+//   };
+//   onLeaveFeedback =( name) => {
+//     this.setState((prevState) => {
+//      return {[name] : prevState[name] + 1}
+//     })
+//   }
+
+//   onCountTotalFeedback = () => {
+//     return (
+//       this.state.good +
+//       this.state.neutral +
+//       this.state.bad
+//     );
+//   };
+//   onCountPositiveFeedbackPercentage = () => {
+//     const total =
+//       this.state.good +
+//       this.state.neutral +
+//       this.state.bad;
+//     const precentage = Math.round((this.state.good / total) * 100);
+//     return `${precentage} %`;
+//   };
+
+//   render() {
+//     return (
+//       <>
+//         <FeedbackOptions
+//           onLeaveFeedback={this.onLeaveFeedback}
+//           options={Object.keys(this.state)}
+//         />
+//         {this.state.good +
+//           this.state.neutral +
+//           this.state.bad >
+//         0 ? (
+//           <Statistics
+//             good={this.state.good}
+//             neutral={this.state.neutral}
+//             bad={this.state.bad}
+//             total={this.onCountTotalFeedback()}
+//             positivePercentage={this.onCountPositiveFeedbackPercentage()}
+//           />
+//         ) : (
+//           <Title titleText="There is no feedback" />
+//         )}
+//       </>
+//     );
+//   }
+// }
--- src/components/molecules/Statistics/Statistics.jsx
+++ src/components/molecules/Statistics/Statistics.jsx
@@ -1,34 +1,60 @@
-import React, { Component } from 'react';
+import React from 'react';
 import proptypes from 'prop-types';
 import Title from 'components/atoms/Title/Title';
 
-class Statistics extends Component {
-  render() {
-    const { good, neutral, bad, total, positivePercentage } = this.props;
-    return (
-      <>
-        <Title fontWeight="600" fontSize="33px" titleText="Statistics" />
-        <Title fontWeight="300" fontSize="22px" titleText={`Good : ${good}`} />
-        <Title
-          fontWeight="300"
-          fontSize="22px"
-          titleText={`Neutral : ${neutral}`}
-        />
-        <Title fontWeight="300" fontSize="22px" titleText={`Bad : ${bad}`} />
-        <Title
-          fontWeight="300"
-          fontSize="22px"
-          titleText={`Total : ${total}`}
-        />
-        <Title
-          fontWeight="300"
-          fontSize="22px"
-          titleText={`PositivePercentage : ${positivePercentage}`}
-        />
-      </>
-    );
-  }
+
+const Statistics = ({good,neutral,bad, total, positivePercentage}) => {
+  
+  return (
+          <>
+            <Title fontWeight="600" fontSize="33px" titleText="Statistics" />
+            <Title fontWeight="300" fontSize="22px" titleText={`Good : ${good}`} />
+            <Title
+              fontWeight="300"
+              fontSize="22px"
+              titleText={`Neutral : ${neutral}`}
+            />
+            <Title fontWeight="300" fontSize="22px" titleText={`Bad : ${bad}`} />
+            <Title
+              fontWeight="300"
+              fontSize="22px"
+              titleText={`Total : ${total}`}
+            />
+            <Title
+              fontWeight="300"
+              fontSize="22px"
+              titleText={`PositivePercentage : ${positivePercentage}`}
+            />
+          </>
+        );
 }
+// class Statistics extends Component {
+//   render() {
+//     const { good, neutral, bad, total, positivePercentage } = this.props;
+//     return (
+//       <>
+//         <Title fontWeight="600" fontSize="33px" titleText="Statistics" />
+//         <Title fontWeight="300" fontSize="22px" titleText={`Good : ${good}`} />
+//         <Title
+//           fontWeight="300"
+//           fontSize="22px"
+//           titleText={`Neutral : ${neutral}`}
+//         />
+//         <Title fontWeight="300" fontSize="22px" titleText={`Bad : ${bad}`} />
+//         <Title
+//           fontWeight="300"
+//           fontSize="22px"
+//           titleText={`Total : ${total}`}
+//         />
+//         <Title
+//           fontWeight="300"
+//           fontSize="22px"
+//           titleText={`PositivePercentage : ${positivePercentage}`}
+//         />
+//       </>
+//     );
+//   }
+// }
 
 Statistics.propTypes = {
   good: proptypes.number,
